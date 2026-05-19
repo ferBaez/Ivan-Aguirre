b1e7bcd9094808794a99804576da3362b4a8b1de
ferBaez <68936190+ferBaez@users.noreply.github.com>
2026-05-18 23:03:47 -0600

    feat: Configure vite for relative paths
    Updates the Vite configuration to use relative paths (`./`) for the base URL. This ensures that static assets are correctly loaded when deployed to GitHub Pages, resolving issues with image loading.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Ivan-Aguirre/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
